9c8833e88b2f1796d8530f88f4de70022c77a5d4
Ezra <dulaneyezra@gmail.com>
2024-08-12 17:45:58 -0400

    base functionality of signup almost complete

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login/Login';
 import NavBar from './components/NavBar/NavBar';
+import Register from './components/Register/Register';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <NavBar/>
     <Routes>
       <Route path='/' element={<Login/>}/>
+      <Route path='/sign-up' element={<Register/>}/>
     </Routes>
     </>
   );
