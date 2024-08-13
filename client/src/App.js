cb96b6a9616f3eac3076e5439b2b49f493b422cb
Brian <brijump3@gmail.com>
2024-08-13 11:54:58 -0500

    stuff

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,7 @@ import RequestModal from "./components/request.jsx"
 import Profile from './components/Profile.jsx'
 import Calender from './components/Calender.js'
 import { ChakraProvider } from '@chakra-ui/react'
-<<<<<<< HEAD
-import Profile from "./components/Profile.jsx"
-=======
 import Register from './components/Register/Register.js';
->>>>>>> 25d87690114a9c542ed3f41af9cab6f53bc6be37
 
 function App() {
   return (
@@ -19,16 +15,12 @@ function App() {
     <NavBar/>
     <Routes>
       <Route path='/' element={<Login/>}/>
-<<<<<<< HEAD
-      <Route path='/profile' element={<Profile />}/>
-=======
       <Route path='/sign-up' element={<Register/>}/>
-<<<<<<< HEAD
+{/* <<<<<<< HEAD */}
       <Route path='/calender' element={<Calender/>}/>
       <Route path='/profile' element={<Profile/>}/>
-=======
->>>>>>> 25d87690114a9c542ed3f41af9cab6f53bc6be37
->>>>>>> bd2d893977fdf0a3b4e024ddaa35adb165d2f5f1
+{/* =======
+>>>>>>> bd2d893977fdf0a3b4e024ddaa35adb165d2f5f1 */}
     </Routes>
       {/* <Profile /> */}
     </>
