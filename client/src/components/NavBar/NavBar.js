a5b0e196dccfd077d0b4d9d77e3fe47259914844
Ezra <dulaneyezra@gmail.com>
2024-08-13 11:37:52 -0400

    initial file

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,16 +1,48 @@
-import { Divider, Flex, Heading } from "@chakra-ui/react";
+import { Avatar, Divider, Flex, Heading, IconButton } from "@chakra-ui/react";
 import ToggleTheme from "./ToggleTheme";
+import {Link} from 'react-router-dom';
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { useDisclosure, Button, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, Input, DrawerFooter, Text } from "@chakra-ui/react";
+import { useRef } from "react";
 
 export default function NavBar() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = useRef()
     return(
         <>
         <Flex padding="1rem" justifyContent="space-between">
-            <Heading>SiteName</Heading>
+            <Heading><Link to="/">PlaceHolder</Link></Heading>
             <Flex>
-                <ToggleTheme/>
+                <IconButton onClick={onOpen} ref={btnRef} isRound={true} icon={<HamburgerIcon/>}/>
             </Flex>
         </Flex>
         <Divider marginBottom="2em" />
+        <Drawer
+          isOpen={isOpen}
+          placement='right'
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>
+                <Avatar/> 
+                <Text>Ezra</Text>
+                </DrawerHeader>
+  
+            <DrawerBody>
+              <Input placeholder='Type here...' />
+            </DrawerBody>
+  
+            <DrawerFooter>
+              <Button variant='outline' mr={3} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme='blue'>Save</Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
         </>
     )
 }
