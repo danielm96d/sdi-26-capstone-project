d79959daa8eb1bd56bba160f73977fb11b5b6cbe
Ezra <dulaneyezra@gmail.com>
2024-08-13 10:29:49 -0400

    removes ChakraProvider from App.js as it is already in Index.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,18 @@ import './App.css';
 import Login from './components/Login/Login';
 import NavBar from './components/NavBar/NavBar';
 import RequestModal from "./components/request.jsx"
-import { ChakraProvider } from '@chakra-ui/react'
 import Register from './components/Register/Register.js';
 
 function App() {
   return (
-    <ChakraProvider>
     <>
-    <NavBar/>
-    <Routes>
-      <Route path='/' element={<Login/>}/>
-      <Route path='/sign-up' element={<Register/>}/>
-    </Routes>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/sign-up' element={<Register />} />
+      </Routes>
       {/* <Profile /> */}
     </>
-    </ChakraProvider>
   );
 }
 
