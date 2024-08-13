d765578168c1084f25f5109a682ff487e48a6702
Ezra <dulaneyezra@gmail.com>
2024-08-13 12:01:32 -0400

    fixes fixed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login/Login';
 import NavBar from './components/NavBar/NavBar';
-import RequestModal from "./components/request.jsx"
 import Profile from './components/Profile.jsx'
 import Calender from './components/Calender.js'
-import { ChakraProvider } from '@chakra-ui/react'
-import Profile from "./components/Profile.jsx"
 import Register from './components/Register/Register.js';
 
 function App() {
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/sign-up' element={<Register />} />
-      </Routes>
-    <NavBar/>
     <Routes>
       <Route path='/' element={<Login/>}/>
       <Route path='/profile' element={<Profile />}/>
