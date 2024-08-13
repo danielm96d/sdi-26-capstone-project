8828a26154d1a3c05bd2d97b52441cd3aa405a0b
Ezra <dulaneyezra@gmail.com>
2024-08-13 16:24:05 -0400

    fixes

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -13,9 +13,9 @@ export default function NotFound() {
 
                     <Heading>404 Not Found</Heading>
                     <Center>
-                        <Avatar size="2xl" src="./elonez.jpg" />
+                        <Avatar size="2xl" src={process.env.PUBLIC_URL + '/elonez.jpg'} />
                     </Center>
-                    <Text>This is not the page you are looking for! Try going <Link color="teal.400" href="/">home</Link></Text>
+                    <Text>{window.location.pathname} is not the page you are looking for! Try going <Link color="teal.400" href="/">home</Link></Text>
                 </Stack>
             </Box>
         </>
