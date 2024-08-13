1e2177f78bb53f61e88106200ac26263c46ab516
Ezra <dulaneyezra@gmail.com>
2024-08-13 14:37:39 -0400

    Mostly resolve conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import Login from './components/Login/Login';
 import NavBar from './components/NavBar/NavBar';
-import RequestModal from "./components/request.jsx"
 import Profile from './components/Profile.jsx'
-import Calendar from './components/Calender.js'
+import Calender from './components/Calender.js'
 import Register from './components/Register/Register.js';
 
 function App() {
-  return (
-    <>
-    <NavBar/>
-    <Routes>
-      <Route path='/' element={<Login/>}/>
-      <Route path='/sign-up' element={<Register/>}/>
-      <Route path='/calendar' element={<Calendar/>}/>
-      <Route path='/profile' element={<Profile/>}/>
-    </Routes>
-    </>
-  );
+      return (
+            <>
+                  <NavBar />
+                  <Routes>
+                        <Route path='/' element={<Login />} />
+                        <Route path='/profile' element={<Profile />} />
+                        <Route path='/sign-up' element={<Register />} />
+                        <Route path='/calender' element={<Calender />} />
+                  </Routes>
+                  {/* <Profile /> */}
+            </>
+      );
 }
 
 export default App;
