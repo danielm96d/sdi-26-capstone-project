b38102dc5bd344589731f997f4868dcf66530797
jholdgate <jholdgate1@yahoo.com>
2024-08-13 14:02:36 -0400

    Implement all position CRUD functionality

--- api/src/migrations/20240812194700_create_events_table.js
+++ api/src/migrations/20240812194700_create_events_table.js
@@ -25,29 +25,5 @@ exports.down = function(knex) {
   return knex.schema.dropTableIfExists('events');
 };
 
-// await knex('events').insert([
-//  {
-//    name: 'Smith Funeral',
-//    startTime: '12:00:00', endTime: '12:00:00',
-//    startDate: '2024-08-26', endDate:'2024-08-26',
-//    description: 'RIP in Peace',
-//    type: 'Funeral',
-//    approved: 'true'
-//  },
-//  {
-//    name: 'Jones Retirement',
-//    startTime: '09:00:00', endTime: '10:00:00',
-//    startDate: '2024-08-28', endDate:'2024-08-28',
-//    description: 'About time',
-//    type: 'Retirement',
-//    approved: 'false'
-//  },
-//  {
-//    name: 'Ralph Nader Inaguration',
-//    startTime: '07:00:00', endTime: '16:00:00',
-//    startDate: '2025-01-20', endDate:'2025-01-20',
-//    description: 'long day',
-//    type: 'Inaguration',
-//    approved: 'true'
-//  },
-// ]);
+// table.increments('id').primary();
+
