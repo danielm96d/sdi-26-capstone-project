6dde881f4561d30d760f1a43f31f14d7cfb7cf5b
jholdgate <jholdgate1@yahoo.com>
2024-08-13 11:27:38 -0400

    Complete User CRUD functionality

--- api/src/app.js
+++ api/src/app.js
@@ -17,6 +17,9 @@ app.get("/", (req, res)=>{
   res.send(`application running using NODE_ENV: ${process.env.NODE_ENV}`);//this line will need editing for deployment
 })
 
+
+//======================================USERS CRUD===========================================\\
+//------------------READ (all and by id)-------------------\\
 app.get("/users*", ( req, res ) => {
   const {id} = req.query
   console.log('id: ', id);
@@ -45,6 +48,7 @@ app.get("/users*", ( req, res ) => {
   }
 });
 
+//------------------CREATE-------------------\\
 app.post("/users", (req, res) => {
   const newUser = req.body;
 
@@ -60,8 +64,49 @@ app.post("/users", (req, res) => {
     });
 });
 
+//------------------DELETE (by id)-------------------\\
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
 
+  knex('users')
+    .where({ id: id})
+    .del()
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).send(`User with ${id} deleted successfully`);
+      } else {
+        res.status(404).send(`User with id ${id} not found`);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send(`Error deleting user: ${err}`);
+    });
+})
 
+//------------------UPDATE(by id)-------------------\\
+app.patch("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+
+  knex('users')
+    .where({ id: id })
+    .update(updates)
+    .returning("*")
+    .then((updateUser) => {
+      if (updateUser.length) {
+        res.status(200).json(updateUser[0]);
+      } else {
+        res.status(404).send(`User with id ${id} not found`);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send(`Error updating user: ${err}`);
+    });
+});
+
+//=====================================Events CRUD===========================================\\
 
 app.listen(PORT, () => {
   console.log(`application running using NODE_ENV: ${process.env.NODE_ENV}`);//this line will need editing for deployment
@@ -94,4 +139,54 @@ app.listen(PORT, () => {
 //         res.status(301).send("Error retrieving single event");
 //       })
 //   }
+// });
+
+// app.post("/events", (req, res) => {
+//   const newUser = req.body;
+
+//   knex('events')
+//     .insert(newEvent)
+//     .returning('*')
+//     .then((insertedEvent) => {
+//       res.status(201).json(insertedEvent[0]);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).send(`Error creating new event: ${err}`);
+//     });
+// });
+
+// app.put('/events/:id', (req, res) => {
+//   const updatedEvent = req.body;
+//   knex('events')
+//     .where({ id: req.params.id })
+//     .update(updatedEvent)
+//     .then((updatedCount) => {
+//       if (updatedCount) {
+//         res.json({ message: 'Event updated successfully' });
+//       } else {
+//         res.status(404).json({ error: 'Event not found' });
+//       }
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(400).send(`Error updating Event: ${err}`);
+//     });
+// });
+
+// app.delete('/events/:id', (req, res) => {
+//   knex('events')
+//     .where({ id: req.params.id })
+//     .del()
+//     .then((deletedCount) => {
+//       if (deletedCount > 0) {
+//         res.json({ message: 'Event deleted successfully' });
+//       } else {
+//         res.status(404).json({ error: 'Event not found' });
+//       }
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(400).send(`Error deleting event: ${err}`);
+//     });
 // });
--- api/src/seeds/0_users.js
+++ api/src/seeds/0_users.js
@@ -1,13 +1,14 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('users').del()
   await knex('users').insert([
     {name: 'John', rank: 'E1', password: '1234', username: 'A1', isApprover:false},
-    {name: 'John', rank: 'E1', password: '1234', username: 'A1', isApprover:false},
-    {name: 'John', rank: 'E1', password: '1234', username: 'A1', isApprover:false}
+    {name: 'Frank', rank: 'E2', password: '1234', username: 'A2', isApprover:false},
+    {name: 'Bob', rank: 'E3', password: '1234', username: 'A3', isApprover:false}
   ]);
 };
+
