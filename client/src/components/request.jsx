89cd2447c3a82284a623d986be50967f8631ef1d
Brian <brijump3@gmail.com>
2024-08-14 11:04:09 -0500

    fleshed out the API functionality for modal

--- client/src/components/request.jsx
+++ client/src/components/request.jsx
@@ -3,59 +3,100 @@ import {
   Modal,
   ModalOverlay,
   ModalContent,
-  ModalHeader,
   ModalFooter,
   ModalBody,
   ModalCloseButton,
   Button,
   FormControl,
   FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-  Card
+  Card,
+  useToast
 } from '@chakra-ui/react'
-import { useDisclosure, Lorem, Select, Input } from '@chakra-ui/react'
-const requestServer = 'http://localhost:8080/'
+import { useDisclosure, Select, Input } from '@chakra-ui/react'
+
+const requestServer = 'http://localhost:8080/events'
 
 function RequestModal() {
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const [type, setType] = useState("apt");
+  const [type, setType] = useState("Retirement");
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [startTime, setStartTime] = useState(null);
   const [endTime, setEndTime] = useState(null);
   const [desc, setDesc] = useState(null);
+  const [submitted, setSubmitted] = useState(0);
+  const toast = useToast();
 
-  let types = ['apt', 'other', 'something else']
+  let types = ['Funeral', 'Retirement', 'Inauguration', 'Other']
 
   const submitFunction = async (e) => {
     e.preventDefault();
-    onClose();
+
     console.log("Type: " + type + "\n")
     console.log("Start Date/Time: " + `${startDate}T${startTime}` +"\n")
-    console.log("End Date: " + `${endDate}T${endTime}` +"\n")
+    console.log("End Date/Time: " + `${endDate}T${endTime}` +"\n")
     console.log("desc: " + desc + "\n")
-    //T00:00
-    try{
-      await fetch(requestServer, {
-        method: 'POST',
-        credentials: "include",
-        headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          type: type,
-          start:`${startDate}T${startTime}`,
-          end: `${endDate}T${endTime}`,
-          desc: desc
-        })
+    if (!startDate || !startTime || !endDate || !endTime){
+      toast({
+        title: 'info.',
+        description: "invalid entry",
+        status: 'error',
+        duration: 4000,
+        isClosable: true,
       })
-    }
-    catch(error){
-      console.log(error)
+    } else {
+      if (startDate > endDate){
+        toast({
+          title: 'info.',
+          description: "start date is after end date",
+          status: 'error',
+          duration: 4000,
+          isClosable: true,
+        })
+      } else {
+        try{
+          let response = await fetch(requestServer, {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json, text/plain, */*',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              name: type,
+              type: type,
+              startTime: startTime,
+              endTime: endTime,
+              startDate: startDate,
+              endDate: endDate,
+              description: desc,
+            })
+          })
+          console.log(response.statusText)
+          toast({
+            title: 'info.',
+            description: response.statusText,
+            status: 'success',
+            duration: 4000,
+            isClosable: true,
+          })
+        }
+        catch(error){
+          console.log(error)
+        }
+        setSubmitted(submitted => submitted + 1);
+        onClose();
+      }
     }
   }
+
+  useEffect(() => {
+      setType("Retirement")
+      setDesc(null)
+      setEndTime(null)
+      setStartTime(null)
+      setEndDate(null)
+      setStartDate(null)
+  }, [submitted])
 
   return (
     <>
@@ -89,15 +130,12 @@ function RequestModal() {
               <Card>
                 <FormLabel>Description</FormLabel>
                 <Input type='text' onChange={(e)=> setDesc(e.target.value)}/>
-              </Card>
+              </Card><br/ >
+                <Button colorScheme='blue' mr={3} onClick={(e)=>submitFunction(e)} type='submit'>
+                Submit
+              </Button>
             </FormControl>
           </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={(e)=>submitFunction(e)} type='submit'>
-              Submit
-            </Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
