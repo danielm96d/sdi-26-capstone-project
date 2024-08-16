0b6013649610774e83cd12a1f04a2fca39c5a6fa
danielm96d <danielthommiller@gmail.com>
2024-08-16 11:11:55 -0600

    testing the events api call, seems to be working now with event details, package.json needed to be reverted due to issues with connection add logout functionality to the navbar for interim testing; added hashed passwords for current users, all seed data now works with the users

--- api/seeds/01_users.js
+++ api/seeds/01_users.js
@@ -1,20 +1,28 @@
+const bcrypt = require('bcryptjs');
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
+
+
 exports.seed = async function(knex) {
+  const testPassword = '1234'
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(testPassword, salt);
   // Deletes ALL existing entries
   await knex('users').del()
   const users = await knex('users').insert([
-    {name: 'John', rank: 'E1', password: '1234', username: 'A1', isApprover:true},
-    {name: 'Frank', rank: 'E2', password: '1234', username: 'A2', isApprover:false},
-    {name: 'Frank', rank: 'E2', password: '1234', username: 'A3', isApprover:false},
-    {name: 'Bob', rank: 'E3', password: '1234', username: 'A4', isApprover:true},
-    {name: 'Dan', rank: 'O1', password: '1234', username: 'A5', isApprover:true},
-    {name: 'Billy', rank: 'O1', password: '1234', username: 'A6', isApprover:true},
-    {name: 'Shawn', rank: 'O1', password: '1234', username: 'A7', isApprover:true},
-    {name: 'Yogi', rank: 'O1', password: '1234', username: 'A8', isApprover:true},
-    {name: 'Peterson', rank: 'O1', password: '1234', username: 'A9', isApprover:true},
+    {name: 'John', rank: 'E1', password: hashedPassword, username: 'A1', isApprover:true},
+    {name: 'Frank', rank: 'E2', password: hashedPassword, username: 'A2', isApprover:false},
+    {name: 'Frank', rank: 'E2', password: hashedPassword, username: 'A3', isApprover:false},
+    {name: 'Bob', rank: 'E3', password: hashedPassword, username: 'A4', isApprover:true},
+    {name: 'Dan', rank: 'O1', password: hashedPassword, username: 'A5', isApprover:true},
+    {name: 'Billy', rank: 'O1', password: hashedPassword, username: 'A6', isApprover:true},
+    {name: 'Shawn', rank: 'O1', password: hashedPassword, username: 'A7', isApprover:true},
+    {name: 'Yogi', rank: 'O1', password: hashedPassword, username: 'A8', isApprover:true},
+    {name: 'Peterson', rank: 'O1', password: hashedPassword, username: 'A9', isApprover:true},
+    {name: 'Daniel Miller', rank: 'O2', password: hashedPassword, username: 'fireball96d@gmail.com', isApprover:false},
 
   ]).returning('id');
 
